606f754267223913f1ae0a9b78a0b312a46b7c42
OMAR MBENGUE <74561202+DokJS@users.noreply.github.com>
2022-06-23 12:53:55 +0000

    feature(Heroe): add html & css structure
    BREAKING CHANGE: Add entire html and css but its not currently responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Navbar from './Components/Navbar';
+import Navbar from './Components/Navbar/Navbar';
+import Heroe from './Components/Heroe/Heroe';
 
 const App = () => {
   return (
     <>
-    <Navbar/>
+      <Navbar />
+      <Heroe />
     </>
   )
 }
