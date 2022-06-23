99ea82e6b0b302e8e7ed1bbe92f3c101dea7d740
OMAR MBENGUE <74561202+DokJS@users.noreply.github.com>
2022-06-23 11:46:05 +0000

    feature(Navbar): add click handler
    BREAKING CHANGE: add click handler on hamburger menu for display menu for small device

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Components/Navbar';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+    <>
+    <Navbar/>
+    </>
+  )
 }
-
 export default App;
